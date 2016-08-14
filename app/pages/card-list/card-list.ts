b2316f8ee22c498d04dc77fa94d43c1215d0a555
Ashik Alam <aalam@webmd.net>
2016-08-13 20:17:31 -0400

    card append works now!!!!

--- app/pages/card-list/card-list.ts
+++ app/pages/card-list/card-list.ts
@@ -13,34 +13,28 @@ import {DeckBrewService} from "../../providers/deck-brew-service/deck-brew-servi
     providers: [DeckBrewService]
 })
 export class CardListPage {
-    cards:Array<any>;
+    // cards:Array<any>;
+    cards : any[];
+
+    page:number;
 
     constructor(private navCtrl:NavController, private deckBrewService:DeckBrewService) {
+        this.page = 0;
 
-        // this.deckBrewService.getCardsSetEMN();
-
-        this.deckBrewService.getCards().subscribe(data => {
+        //TODO: make this live template
+        this.deckBrewService.getCards(this.page).subscribe(data => {
             console.log('111hello!');
+            // this.cards = data;
             this.cards = data;
+
+            // this.cards.push(data);
             console.log(this.cards);
         }, (err) => {
             console.log(err);
         });
 
-        
-        // this.deckBrewService.getCards().then( res => {
-        //     console.log('hello!');
-        //     this.cards = res;
-        //     console.log(res);
-        //
-        //
-        //     }
-        // );
-        //      console.log('hello!3333');
-        //  }
-        //
-
     }
+
 
     doInfinite(infiniteScroll) {
         console.log('Begin async operation');
@@ -49,9 +43,36 @@ export class CardListPage {
             // for (var i = 0; i < 30; i++) {
             //     this.items.push( this.items.length );
             // }
+            this.page++;
 
-            console.log('Async operation has ended');
-            infiniteScroll.complete();
+            this.deckBrewService.getCards(this.page).subscribe(data => {
+                console.log('start suscribe');
+                console.log(this.cards);
+                console.log('logging data requested');
+                console.log(data);
+                if (data.length < 1) {
+                    console.log('empty array, disable!');
+                    //TODO: show toast
+                    infiniteScroll.enable(false);
+                } else {
+                    console.log('pushing cards!');
+                    // this.cards.pop();
+
+                    console.log(this.cards.length);
+                    this.cards = this.cards.concat(data);
+                    console.log(data.length);
+                    console.log(this.cards.length);
+                    this.cards = this.cards.slice();
+                    infiniteScroll.complete();
+
+                }
+            }, (err) => {
+                console.log(err);
+            });
+            //
+            // console.log('Async operation has ended');
+            // infiniteScroll.complete();
+
         }, 500);
     }
 }
--- app/providers/deck-brew-service/deck-brew-service.ts
+++ app/providers/deck-brew-service/deck-brew-service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
 import "rxjs/add/operator/map";
+import {Observable} from "rxjs/Rx";
 
 /*
  Generated class for the DeckBrewService provider.
@@ -20,12 +21,34 @@ export class DeckBrewService {
     }
 
 
-    getCards() {
-        var url = 'https://api.deckbrew.com/mtg/cards?set=emn';
+    getCards(page) {
+        var url = `https://api.deckbrew.com/mtg/cards?set=emn&page=${page}`;
         console.log('request MADE!!!');
              return this.http.get(url).map(res => res.json());
 
     }
+
+    public getCards2(page: number): Observable<any> {
+        return this.http.get(`https://api.deckbrew.com/mtg/cards?set=emn&page=${page}`)
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any) {
+        // In a real world app, we might use a remote logging infrastructure
+        // We'd also dig deeper into the error to get a better message
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg); // log to console instead
+        return Observable.throw(errMsg);
+    }
+    // getCards(page) {
+    //     page
+    //     var url = 'https://api.deckbrew.com/mtg/cards?set=emn';
+    //     console.log('request MADE!!!');
+    //     return this.http.get(url).map(res => res.json());
+    //
+    // }
 
 
 
